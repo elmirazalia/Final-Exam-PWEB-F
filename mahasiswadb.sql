6051a81969c909ec799b694a18043ce8f3edd684
elmirazalia <144777584+elmirazalia@users.noreply.github.com>
2024-12-16 00:00:29 +0700

    Update mahasiswadb.sql

--- mahasiswadb.sql
+++ mahasiswadb.sql
@@ -1,28 +1,7 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versi server:                 10.4.28-MariaDB - mariadb.org binary distribution
--- OS Server:                    Win64
--- HeidiSQL Versi:               12.5.0.6677
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Membuang struktur basisdata untuk mahasiswadb
-DROP DATABASE IF EXISTS `mahasiswadb`;
-CREATE DATABASE IF NOT EXISTS `mahasiswadb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+CREATE DATABASE IF NOT EXISTS `mahasiswadb`;
 USE `mahasiswadb`;
 
--- membuang struktur untuk table mahasiswadb.mahasiswa
-DROP TABLE IF EXISTS `mahasiswa`;
-CREATE TABLE IF NOT EXISTS `mahasiswa` (
+CREATE TABLE IF NOT EXISTS `mahasiswas` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Nama` varchar(100) NOT NULL,
   `NRP` varchar(20) NOT NULL,
@@ -31,10 +10,6 @@ CREATE TABLE IF NOT EXISTS `mahasiswa` (
   UNIQUE KEY `NRP` (`NRP`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Pengeluaran data tidak dipilih.
-
--- membuang struktur untuk table mahasiswadb.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Username` varchar(50) NOT NULL,
@@ -42,10 +17,5 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Pengeluaran data tidak dipilih.
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+SHOW TABLES;
