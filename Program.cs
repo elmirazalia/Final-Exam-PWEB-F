6689aa62f3f7cf3422f7da6bb572528d5df0a801
elmirazalia <144777584+elmirazalia@users.noreply.github.com>
2024-12-15 23:42:22 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using CRUD_mahasiswa.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Mahasiswas}/{action=Index}/{id?}");
+
+app.Run();
